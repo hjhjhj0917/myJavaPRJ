b77f2567ef310fdc9b90045a9a811a3cec86e9d1
hjhjhj0917 <abc22842199@gmail.com>
2025-03-14 14:53:59 +0900

    수정사항

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    /*
+     *public static void main(String[] args) 함수는 자바에서 가장 먼저 실행되는 함수
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello!! My First java Coding!!");
+        System.out.println("Hello!! My Second java Coding!!");
+        System.out.println("Hello!! My Third java Coding!!");
+        System.out.println("Hello!! My fourth java Coding!!");
+        System.out.println("Hello!! My fifth java Coding!!");
+        System.out.println("Hello!! My second java Coding!!");
+        System.out.println("Hello!! My third java Coding!!");
+    }
+}
