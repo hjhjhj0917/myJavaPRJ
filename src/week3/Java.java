dfece0d4d9ceb7ab8a684ab4ba6225d0601a4665
hjhjhj0917 <abc22842199@gmail.com>
2025-04-16 08:47:41 +0900

    특강

---
+++ src/week3/Java.java
@@ -0,0 +1,9 @@
+package week3;
+
+public class Java {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.setPosition(1,1);
+        System.out.println(rabbit.getX());
+    }
+}
